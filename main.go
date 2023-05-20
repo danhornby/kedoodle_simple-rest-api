caac5a059c1c6e94cf26880d2bf8ce13211c952e
Keddy Shen <hello@keddy.dev>
2023-05-20 16:58:14 +1000

    create status handler and add to router

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import "github.com/gin-gonic/gin"
 
 func setupRouter(r *gin.Engine) {
 	r.GET("/", helloWorldHandler)
+	r.GET("/status", statusHandler)
 }
 
 func helloWorldHandler(c *gin.Context) {
 	c.String(200, "Hello World")
+}
+
+func statusHandler(c *gin.Context) {
+	c.String(200, "OK")
 }
 
 func main() {
